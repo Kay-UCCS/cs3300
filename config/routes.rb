4e1a6d8aedbc15401768e7ffe7eebcacbfe0ef94
Your Name <you@example.com>
2021-04-24 21:11:04 +0000

    Migrated heroku, heroku works now

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   root "projects#index"
   resources :projects
-end
+endher
 
 Rails.application.routes.draw do
   resources :projects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
 
 
 
