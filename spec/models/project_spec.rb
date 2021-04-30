098ea73d952032eeb085bcf10a0e3869b20af40c
Your Name <you@example.com>
2021-04-30 02:25:30 +0000

    Added scope test

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -22,3 +22,23 @@ RSpec.describe Project, type: :model do
 
   end
 end
+
+require "rails_helper"
+
+RSpec.describe Project, type: :model do
+  # ...
+
+  context "scopes tests" do
+    let(:params) { { title: "Title", description: "some description" } }
+    before(:each) do
+      Project.create(params)
+      Project.create(params)
+      Project.create(params)
+    end
+
+    it "should return all projects" do
+      expect(Project.count).to eq(3)
+    end
+
+  end
+end
